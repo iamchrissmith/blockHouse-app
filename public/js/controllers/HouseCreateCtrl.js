8421764c056ebdd6c3399d38a2d321834d93af4f
Chris Smith <me@chrisryansmith.com>
2017-09-16 09:49:32 -0600

    add contract call to create houses in HouseCreateCtrl

--- public/js/controllers/HouseCreateCtrl.js
+++ public/js/controllers/HouseCreateCtrl.js
@@ -1,17 +1,35 @@
 angular.module('HouseCreateCtrl', [])
-  .controller('CreateHouseController', function($location, $scope, $routeParams, HouseService) {
+  .controller('CreateHouseController', function($location, $scope, $routeParams, HouseService, $rootScope) {
     $scope.house = {};
+
+    if (!$rootScope.isAdmin) {
+      $location.url('/houses');
+    }
     
     $scope.createHouse = () => {
       const newHouse = $scope.house;
+      if (!$scope.house.price)
+        newHouse.price = 0;
       $scope.house = {};
-      newHouse.address = "0x000"; // need to get the real address from the blockchain.
-      HouseService.create(newHouse)
-        .then( response => {
-          console.log(response.data);
-          $location.url(`/houses/${response.data.id}`);
-        }, err => {
-          $scope.status = `Unable to load house data: ${err.message}`;
+      $rootScope.contract.isAdmin($rootScope.selectedAccount, {from:$rootScope.selectedAccount})
+        .then( _isAdmin => {
+          if(!_isAdmin)
+            return;
+
+          return $rootScope.contract.newHouse(
+                                newHouse.owner,
+                                newHouse.price,
+                                newHouse.forSale,
+                                {from: $rootScope.selectedAccount, gas: 4000000});
+        })
+        .then( tx => {
+          newHouse.address = tx.logs[0].args.house; // need to get the real address from the blockchain.
+          HouseService.create(newHouse)
+          .then( response => {
+            $location.url(`/houses/${response.data.id}`);
+          }, err => {
+              $scope.status = `Unable to load house data: ${err.message}`;
+          });
         });
     };
   });
